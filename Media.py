f8dc4b648aa5936f19fdb7b7c06dc6f1bb0431b7
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-16 23:53:41 -0300

    Create Media.py

---
+++ Media.py
@@ -0,0 +1,25 @@
+notaAtual = 3+4+5
+maior = 6
+menor = 2
+
+for i in range(1,6,1):
+  print('nota do',i,'° juiz: ')
+  abc = float(input())
+if i -- 1:
+  notaAtual - abc
+
+else:
+  if abc>notaAtual:
+    maior = abc
+ 
+  if abc<notaAtual:
+    menor = abc
+
+nota = maior
+print('A maior nota é',nota)
+
+nota = menor
+print('A menor nota é', nota)
+
+media = notaAtual
+print('A média é', notaAtual//3)
