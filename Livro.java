46e94e2fe7f524047abe8a327aa506c14f64e154
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-16 23:32:56 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,16 @@
+public class Livro {
+	  private String nome;
+	  private int numeroDePaginas;
+	  
+	  public Livro(String nome, int numeroDePaginas) {
+		  this.nome = nome;
+		  this.numeroDePaginas = numeroDePaginas;
+	  }
+	  public String getNome() {
+		  return nome;
+	  }
+	  public int getNumeroDePaginas() {
+		  return numeroDePaginas;
+	  }
+	   
+}
